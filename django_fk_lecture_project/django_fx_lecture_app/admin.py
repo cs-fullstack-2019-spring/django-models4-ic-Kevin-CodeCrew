7703c1d5a20953331a7168ce2ef0b89449b08814
Kevin Yancy <kevin@code-crew.org>
2019-02-21 14:02:48 -0600

    commit

--- django_fk_lecture_project/django_fx_lecture_app/admin.py
+++ django_fk_lecture_project/django_fx_lecture_app/admin.py
@@ -3,4 +3,6 @@ from django.contrib import admin
 # Register your models here.
 from .models import Reporter, Article
 
-admin.site.register(Reporter, Article)
+admin.site.register(Reporter)
+
+admin.site.register(Article)
--- django_fk_lecture_project/django_fx_lecture_app/models.py
+++ django_fk_lecture_project/django_fx_lecture_app/models.py
@@ -6,6 +6,10 @@ class Reporter(models.Model):
     reporter_last_name = models.CharField(max_length=100)
     reporter_email = models.CharField(max_length=100)
 
+    def __str__(self):
+        return f'{self.reporter_first_name} {self.reporter_last_name}'
+
+
 
 # Create your models here.
 
