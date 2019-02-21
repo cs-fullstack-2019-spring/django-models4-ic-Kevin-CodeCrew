66d94c01a71147495c0a351dd1f14aef42193b70
Kevin Yancy <kevin@code-crew.org>
2019-02-21 13:50:10 -0600

    commit

--- django_fk_lecture_project/django_fx_lecture_app/models.py
+++ django_fk_lecture_project/django_fx_lecture_app/models.py
@@ -2,14 +2,14 @@ from django.db import models
 
 
 class Reporter(models.Model):
-    reporter_first_name = models.Charfield(max_length=100)
-    reporter_last_name = models.Charfield(max_length=100)
-    reporter_email = models.Charfield(max_length=100)
+    reporter_first_name = models.CharField(max_length=100)
+    reporter_last_name = models.CharField(max_length=100)
+    reporter_email = models.CharField(max_length=100)
 
 
 # Create your models here.
 
 class Article(models.Model):
-    article_name = models.Charfield(max_length=100)
+    article_name = models.CharField(max_length=100)
     article_pubdate = models.DateField()
     article_reporter = models.ForeignKey(Reporter, on_delete=models.CASCADE)
